71a35583958af00aec4926aa638908d2da3cc6b0
akananthu11 <akananthu11@gmail.com>
2025-05-15 04:50:04 +0530

    feat: terms

---
+++ src/app/terms-and-conditions/layout.tsx
@@ -0,0 +1,12 @@
+export default function TermsLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            {children}
+        </>
+
+    );
+}
